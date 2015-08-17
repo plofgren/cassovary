9b16213c17157636c60dc1cf97a85b4233ec73ff
Peter Lofgren <plofgren@cs.stanford.edu>
2015-08-17 11:13:50 -0700

    Add (temporary?) benchmark

--- cassovary-core/src/main/scala/com/twitter/cassovary/graph/MemoryMappedDirectedGraph.scala
+++ cassovary-core/src/main/scala/com/twitter/cassovary/graph/MemoryMappedDirectedGraph.scala
@@ -6,7 +6,9 @@ import java.nio.file.{FileSystems, Path, StandardOpenOption}
 import java.nio.channels.FileChannel.MapMode
 import java.io.{FileOutputStream, DataOutputStream, BufferedOutputStream, File}
 
-import com.twitter.cassovary.util.io.{MemoryMappedIntLongSource, IntLongSource}
+import com.twitter.cassovary.util.NodeNumberer
+import com.twitter.cassovary.util.io.{AdjacencyListGraphReader, MemoryMappedIntLongSource, IntLongSource}
+import com.twitter.util.Stopwatch
 
 /**
  * A graph which reads edge data from a memory mapped file.  There is no object overhead per node: the memory
@@ -77,7 +79,6 @@ object MemoryMappedDirectedGraph {
     var outboundOffset = 8L + 8L * (n + 1) * 2
     for (i <- 0 until n) {
       out.writeLong(outboundOffset)
-      println(s"node $i's outneighbor index: $outboundOffset")
       outboundOffset += 4 * (graph.getNodeById(i) map (_.outboundCount)).getOrElse(0)
     }
     out.writeLong(outboundOffset) // Needed to compute outdegree of node n-1
@@ -100,5 +101,46 @@ object MemoryMappedDirectedGraph {
       }
     }
     out.close()
+  }
+}
+
+object MemoryMappedDirectedGraphBenchmark {
+  def readGraph(graphPath: String): DirectedGraph[Node] = {
+    val filenameStart = graphPath.lastIndexOf('/') + 1
+    val graphDirectory = graphPath.take(filenameStart)
+    val graphFilename = graphPath.drop(filenameStart)
+    println("loading graph:" + graphFilename)
+
+    val reader = new AdjacencyListGraphReader(
+      graphDirectory,
+      graphFilename,
+      new NodeNumberer.IntIdentity(),
+      _.toInt) {
+      override def storedGraphDir: StoredGraphDir = StoredGraphDir.BothInOut
+    }
+    reader.toArrayBasedDirectedGraph()
+  }
+
+  def main(args: Array[String]): Unit = {
+    var startTime = System.currentTimeMillis()
+    val testNodeId = 30000000
+    val graphName = args(1)
+    if (args(0) == "readAdj") {
+      val graph = readGraph(graphName)
+      println(s"outneighbors of node $testNodeId: " + graph.getNodeById(testNodeId).get.outboundNodes())
+      val loadTime = (System.currentTimeMillis() - startTime) / 1000.0
+      println(s"Time to read adj graph: $loadTime")
+
+      val binaryFileName = graphName.substring(0, graphName.lastIndexOf(".")) + ".dat"
+      startTime = System.currentTimeMillis()
+      MemoryMappedDirectedGraph.graphToFile(graph, new File( binaryFileName))
+      val writeTime = (System.currentTimeMillis() - startTime) / 1000.0
+      println(s"Time to write binary graph: $writeTime")
+    } else if (args(0) == "readBin") {
+      val graph = new MemoryMappedDirectedGraph(new File(graphName))
+      println(s"outneighbors of node $testNodeId: " + graph.getNodeById(testNodeId).get.outboundNodes())
+      val loadTime = (System.currentTimeMillis() - startTime) / 1000.0
+      println(s"Time to read binary graph: $loadTime")
+    }
   }
 }
